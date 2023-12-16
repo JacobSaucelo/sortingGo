6495297980361953077f45ad358d02f5cbf911ac
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-16 20:52:15 +0800

    addded types

--- main.go
+++ main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
+/*
+	new		green
+	worn	yellow
+	broken	red
+*/
+
+type Wear string
+
+const (
+	New    Wear = "green"
+	Worn   Wear = "yellow"
+	Broken Wear = "red"
+)
+
+type Items struct {
+	backpack []ItemType
+}
+
+type ItemType struct {
+	name string
+	wear Wear
+}
+
 func main() {
 	fmt.Print("initial commit")
 }
